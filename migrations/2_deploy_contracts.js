977dad5930244357c448c0a9c302d5061c40014a
jack <jackoelv@qq.com>
2020-01-14 16:57:44 +0800

    more simple

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,11 +11,11 @@ module.exports = function(deployer) {
   // deployer.deploy(KOLMission,{gas: 6700000});
   // deployer.deploy(KOLOffer,{gas: 7700000});KolCoreTeam
 
-  // deployer.deploy(KOLVote,{gas: 6700000});
+  deployer.deploy(KOLVote,{gas: 6700000});
   // deployer.deploy(KolFund,
   //                "0x46Ba0c589c0E0531319809BcA37db878Eb4CC651",
   //                {gas: 7700000});
-  deployer.deploy(KolCoreTeam,
-              "0x46Ba0c589c0E0531319809BcA37db878Eb4CC651",
-              {gas: 7700000});
+  // deployer.deploy(KolCoreTeam,
+  //             "0x46Ba0c589c0E0531319809BcA37db878Eb4CC651",
+  //             {gas: 7700000});
 };
