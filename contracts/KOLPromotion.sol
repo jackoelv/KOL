e9483b3ac384ccf8b26c285c181141edd91e2edd
jack <jackoelv@qq.com>
2020-04-22 13:59:32 +0800

    compile passed

--- contracts/KOLPromotion.sol
+++ contracts/KOLPromotion.sol
@@ -45,8 +45,8 @@ pragma solidity ^0.4.23;
  }
 
  /**
-  * @title KOL Promotion contract
-  * @dev visit: https://github.com/jackoelv/KOL/
+  * title KOL Promotion contract
+  * dev visit: https://github.com/jackoelv/KOL/
  */
 
  contract ERC20Basic {
@@ -64,8 +64,8 @@ pragma solidity ^0.4.23;
  }
 
  /**
-  * @title KOL Promotion contract
-  * @dev visit: https://github.com/jackoelv/KOL/
+  * title KOL Promotion contract
+  * dev visit: https://github.com/jackoelv/KOL/
  */
 
  contract BasicToken is ERC20Basic {
@@ -86,8 +86,8 @@ pragma solidity ^0.4.23;
  }
 
  /**
-  * @title KOL Promotion contract
-  * @dev visit: https://github.com/jackoelv/KOL/
+  * title KOL Promotion contract
+  * dev visit: https://github.com/jackoelv/KOL/
  */
 
  contract StandardToken is BasicToken, ERC20 {
@@ -117,8 +117,8 @@ pragma solidity ^0.4.23;
  }
 
  /**
-  * @title KOL Promotion contract
-  * @dev visit: https://github.com/jackoelv/KOL/
+  * title KOL Promotion contract
+  * dev visit: https://github.com/jackoelv/KOL/
  */
 
  contract Ownable {
@@ -139,8 +139,8 @@ pragma solidity ^0.4.23;
      }
  }
  /**
-  * @title KOL Promotion contract
-  * @dev visit: https://github.com/jackoelv/KOL/
+  * title KOL Promotion contract
+  * dev visit: https://github.com/jackoelv/KOL/
  */
 contract KOLPromote is Ownable{
   using SafeMath for uint256;
@@ -157,45 +157,53 @@ contract KOLPromote is Ownable{
   uint8 public constant userLevel1 = 20;
   uint8 public constant userLevel2 = 10;
 
-  uint8 public constant comLevel1Users = 100;
-  uint8 public constant comLevel2Users = 300;
-  uint8 public constant comLevel3Users = 500;
+  uint16 public constant comLevel1Users = 100;
+  uint16 public constant comLevel2Users = 300;
+  uint16 public constant comLevel3Users = 500;
 
-  uint8 public constant comLevel1Amount = 10000 * (10 ** 18);
-  uint8 public constant comLevel2Amount = 30000 * (10 ** 18);
-  uint8 public constant comLevel3Amount = 50000 * (10 ** 18);
+  uint256 public constant comLevel1Amount = 10000 * (10 ** 18);
+  uint256 public constant comLevel2Amount = 30000 * (10 ** 18);
+  uint256 public constant comLevel3Amount = 50000 * (10 ** 18);
 
   uint8 public constant comLevel1 = 3;
   uint8 public constant comLevel2 = 5;
   uint8 public constant comLevel3 = 10;
+  uint8 public constant withDrawRate = 5;
+
+  uint256 public constant withDrawDays = 30 days;
 
 
-  address[] private inviteAddr;// A->B->C: inviteAddr= B,A
-  address[] private childAddr;// A-->B,A-->C,childAddr= B, C
+  /* address[] private inviteAddr;// A->B->C: inviteAddr= B,A
+  address[] private childAddr;// A-->B,A-->C,childAddr= B, C */
 
-  mapping (address => inviteAddr) InviteList;
-  mapping (address => childAddr) ChildAddrs;
+
 
   struct lock{
     uint256 begin;
     uint256 amount;
     uint256 end;
     bool withDrawed;
+  }
 
-  };
+  struct teamRate{
+    uint8 rate;
+    uint256 changeTime;
+
+  }
 
   struct inviteBonus{
     uint256 begin;//网体开始时间
     uint256 dayBonus;//网体当日加速
     uint256 hisTotalBonus;
-  };
+  }
   struct withDraws{
     uint256 time;
     uint256 amount;
   }
 
-
-
+  mapping (address => address[]) internal InviteList;
+  mapping (address => address[]) internal ChildAddrs;
+  mapping (address => teamRate[]) internal TeamRateList;
   mapping (address => lock[]) internal LockHistory;
   mapping (address => uint256) internal LockBalance;
   mapping (address => uint256) internal LockHistoryBonus;
@@ -216,9 +224,9 @@ contract KOLPromote is Ownable{
 
 
   event Registed(address _user,uint256 inviteCode);
-  event Upgraded(address _user,uint8 _level);
-  event Downgraded(address _user,uint8 _level);
-  event WithDraw(address _user,uint256 _self,uint256,_promotion,uint256 _team);
+  event GradeChanged(address _user,uint8 _oldLevel,uint8 _newLevel);
+  event WithDraw(address _user,uint256 _self,uint256 _promotion,uint256 _team);
+  event WithDrawBalance(address _user,uint256 _balance);
 
 
   constructor(address _tokenAddress,uint256 _begin,uint256 _end) public {
@@ -241,13 +249,14 @@ contract KOLPromote is Ownable{
       _;
   }
   /**
-   * @title 提现KOL到自己的账户
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 提现KOL到自己的账户
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function withDraw(bool _type) public {
     //_type true，提利息，false，提本息
     //提现就把利息全部提走，只能提整数，不能提小数点。
     //如果是提现本金，需要判断一下用户是否满足自由提现的条件。
+    require(LockBalance[msg.sender] > 0);
     uint256 self;
     uint256 promotion;
     uint256 team;
@@ -255,36 +264,63 @@ contract KOLPromote is Ownable{
       //提利息+奖励
       //今天凌晨的时间
       uint256 yestodayLastSecond = now.sub(now.sub(begin) % 86400) - 1;//昨天的最后一秒
-      uint16 days = now.sub(now.sub(begin) % 86400).div(86400);//除法刚好是整数
-      for (uint i = 0 ; i<days; i++) {
+      uint256 lastingDays = now.sub(now.sub(begin) % 86400).div(86400);//除法刚好是整数
+      for (uint i = 0 ; i<lastingDays; i++) {
          self += calcuBonus(msg.sender,yestodayLastSecond);
          promotion += calcuInviteBonus(msg.sender,yestodayLastSecond);
-         team += calcuComBonus(msg.sender,yestodayLastSecond);
+         team += calcuTeamBonus(msg.sender,yestodayLastSecond);
          yestodayLastSecond = yestodayLastSecond.sub(86400);
       }
       //应该把这个历史记录下来？爆出一个事件吧，不记录链上了。
       emit WithDraw(msg.sender,self,promotion,team);
       uint256 total = self.add(promotion).add(team);
+      WithDraws[msg.sender] += total;
       kol.transfer(msg.sender,total);
-
-
     }else{
       //提本金，先判断是否符合条件，一旦提了本金就要注意给上级网体降级减少网体的加速。
+      if ((TotalLockingAmount[msg.sender] < LockBalance[msg.sender].mul(withDrawRate)) &&
+                                    (now < LockHistory[msg.sender][0].begin + withDrawDays)){
+          //没有达到提现要求
+          return;
+      }else{
+        for (uint j = 0; j<LockHistory[msg.sender].length; j++){
+          LockHistory[msg.sender][j].end = now;
+          LockHistory[msg.sender][j].withDrawed = true;
+        }
+        uint256 balance = LockBalance[msg.sender];
+        emit WithDrawBalance(msg.sender,balance);
+        kol.transfer(msg.sender,balance);
+        LockBalance[msg.sender] = 0;
+        afterDraw(msg.sender,balance);//提现以后需要对上级所有的网体人数和金额做减法。
+      }
 
     }
 
   }
+  /**
+   * title 提现以后上级的处理
+   * dev visit: https://github.com/jackoelv/KOL/
+  */
+  function afterDraw(address _addr,uint256 _amount) private{
+    for (uint i = 0; i<InviteList[_addr].length; i++){
+        require(TotalUsers[InviteList[_addr][i]] > 0);
+        TotalUsers[InviteList[_addr][i]] -= 1;
+        //给上面的加入团队总金额
+        TotalLockingAmount[InviteList[_addr][i]] = TotalLockingAmount[InviteList[_addr][i]].sub(_amount);
+        queryAndSetLevelN(InviteList[_addr][i]);
+    }
+  }
 
   /**
-   * @title 获得自己的邀请码
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 获得自己的邀请码
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function getCode() public view returns(uint256) {
     return (RInviteCode[msg.sender]);
   }
   /**
-   * @title 注册并绑定邀请关系
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 注册并绑定邀请关系
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function register(uint256 _fInviteCode) public {
     uint256 random = uint256(keccak256(now, msg.sender)) % 100;
@@ -292,7 +328,7 @@ contract KOLPromote is Ownable{
     iCode = iCode.add(random);
 
     require(InviteCode[_myInviteCode] == address(0));
-    require(InviteCode[_fInviteCode] != address(0))
+    require(InviteCode[_fInviteCode] != address(0));
     InviteCode[_myInviteCode] = msg.sender;
     RInviteCode[msg.sender] = _myInviteCode;
     emit Registed(msg.sender,iCode);
@@ -309,13 +345,13 @@ contract KOLPromote is Ownable{
 
   }
   /**
-   * @title 转入KOL进行持仓生息
-   * @param _usdtOrCoin, true:金本位; false:币本位
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 转入KOL进行持仓生息
+   * _usdtOrCoin, true:金本位; false:币本位
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function join(uint256 _amount,bool _usdtOrCoin) public {
     kol.transferFrom(msg.sender,address(this),_amount);
-    LockHistory[msg.sender].push(now,_amount,0,false);
+    LockHistory[msg.sender].push(lock(now,_amount,0,false));
     LockBalance[msg.sender] = LockBalance[msg.sender].add(_amount);
 
     USDTOrCoin[msg.sender] = _usdtOrCoin;
@@ -326,31 +362,46 @@ contract KOLPromote is Ownable{
         TotalUsers[InviteList[msg.sender][i]] += 1;
       }
       //给上面的加入团队总金额
-      TotalLockingAmount[InviteList[msg.sender][i]] = InviteList[msg.sender][i].add(_amount);
+      TotalLockingAmount[InviteList[msg.sender][i]] = TotalLockingAmount[InviteList[msg.sender][i]].add(_amount);
       queryAndSetLevelN(InviteList[msg.sender][i]);
     }
 
 
   }
   /**
-   * @title 查询并设置用户的身份级别
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 查询并设置用户的身份级别
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function queryAndSetLevelN(address _addr) internal{
     if ((TotalUsers[_addr] >= comLevel3Users) && (TotalLockingAmount[_addr] >= comLevel3Amount)){
-      isLevelN[_addr] = 3;
-      emit Upgraded(_addr, 3);
+      if (isLevelN[_addr]!=3){
+        emit GradeChanged(_addr,isLevelN[_addr],3);
+        isLevelN[_addr] = 3;
+        TeamRateList[_addr].push(teamRate(3,now));
+      }
     }else if((TotalUsers[_addr] >= comLevel2Users) && (TotalLockingAmount[_addr] >= comLevel2Amount)){
-      isLevelN[_addr] = 2;
-      emit Upgraded(_addr, 2);
+      if (isLevelN[_addr]!=2){
+        emit GradeChanged(_addr,isLevelN[_addr],2);
+        isLevelN[_addr] = 2;
+        TeamRateList[_addr].push(teamRate(2,now));
+      }
     }else if((TotalUsers[_addr] >= comLevel1Users) && (TotalLockingAmount[_addr] >= comLevel1Amount)){
-      isLevelN[_addr] = 1;
-      emit Upgraded(_addr, 1);
+      if (isLevelN[_addr]!=1){
+        emit GradeChanged(_addr,isLevelN[_addr],1);
+        isLevelN[_addr] = 1;
+        TeamRateList[_addr].push(teamRate(1,now));
+      }
+    }else{
+      if (isLevelN[_addr]!=0){
+        emit GradeChanged(_addr,isLevelN[_addr],0);
+        isLevelN[_addr] = 0;
+        TeamRateList[_addr].push(teamRate(0,now));
+      }
     }
   }
   /**
-   * @title 用户升级后首次计算历史佣金的汇总
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 用户升级后首次计算历史佣金的汇总
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function firstUpgradeBonus(address _addr) internal{
     if (isLevelN[_addr] == 1){
@@ -362,8 +413,8 @@ contract KOLPromote is Ownable{
     }
   }
   /**
-   * @title 查询到指定时间点，当天用户的持币生息收益，建议输入时间为当天的11点59分
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 查询到指定时间点，当天用户的持币生息收益，建议输入时间为当天的11点59分
+   * dev visit: https://github.com/jackoelv/KOL/
   */
   function calcuBonus(address _addr,uint256 _queryTime) private view returns(uint256) {
     //输入参数为查询的时间。
@@ -389,10 +440,10 @@ contract KOLPromote is Ownable{
 
   }
   /**
-   * @title 查询到指定时间点，计算用户当天的推广收益
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 查询到指定时间点，计算用户当天的推广收益
+   * dev visit: https://github.com/jackoelv/KOL/
   */
-  function calcuInviteBonus(address _addr,uint256 _queryTime) private view returns(uint256,uint256) {
+  function calcuInviteBonus(address _addr,uint256 _queryTime) private view returns(uint256) {
     //直推，直推的直推，比较金额，计算佣金。
     //先计算直推的
     address[] childUser = ChildAddrs[_addr];
@@ -425,46 +476,57 @@ contract KOLPromote is Ownable{
         }
       }
     }
-    return (level1Bonus,level2Bonus);
+    return (level1Bonus.add(level2Bonus));
 
   }
   /**
-   * @title 查询指定时间用户的有效锁仓余额
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 查询指定时间用户的有效锁仓余额
+   * dev visit: https://github.com/jackoelv/KOL/
   */
-  function queryLockBalance(address _addr,uint256 _queryTime) private view return(uint256) {
+  function queryLockBalance(address _addr,uint256 _queryTime) private view returns(uint256) {
     uint256 dayLockBalance;
     for (uint j = 0; j<LockHistory[_addr].length; j++){
-      if (LockHistory[_addr][i].withDrawed){
+      if (LockHistory[_addr][j].withDrawed){
         //如果是已经提现的资金，那就要求计算日期是在起止时间内的。
-        if ((_queryTime >= LockHistory[_addr][i].begin) && (_queryTime <= LockHistory[_addr][i].end)){
-            dayLockBalance += LockHistory[_addr][i].amount;
+        if ((_queryTime >= LockHistory[_addr][j].begin) && (_queryTime <= LockHistory[_addr][j].end)){
+            dayLockBalance += LockHistory[_addr][j].amount;
         }
       }else{
-        if (_queryTime >= LockHistory[_addr][i].begin ){
+        if (_queryTime >= LockHistory[_addr][j].begin ){
           //这个就要计入到当天的收益
-          dayLockBalance += LockHistory[_addr][i].amount;
+          dayLockBalance += LockHistory[_addr][j].amount;
         }
       }
     }
     return dayLockBalance;
   }
   /**
-   * @title 查询并计算用户的网体收益
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 查询并计算用户的网体收益
+   * dev visit: https://github.com/jackoelv/KOL/
   */
-  function calcuComBonus(address _addr,uint256 _queryTime) private view return(uint256) {
+  function calcuTeamBonus(address _addr,uint256 _queryTime) private view returns(uint256) {
     uint8 rate;
-    if (isLevelN[_addr]>0){
-      //先算出来网体收益比
-      if (isLevelN[_addr] == 1){
-        rate = comLevel1;
-      }else if(isLevelN[_addr] == 2){
-        rate = comLevel2;
-      }else if(isLevelN[_addr] == 3){
-        rate = comLevel3;
+    if (TeamRateList[_addr].length == 0){
+      return 0;
+    }else{
+      for (uint i = 0; i<TeamRateList[_addr].length; i++){
+        if (i < TeamRateList[_addr].length -1){
+          if (( _queryTime >= TeamRateList[_addr][i].changeTime ) &&
+                    ( _queryTime <= TeamRateList[_addr][i+1].changeTime )){
+            rate = TeamRateList[_addr][i].rate;
+          }
+        }else{
+          if ( _queryTime >= TeamRateList[_addr][i].changeTime ){
+            rate = TeamRateList[_addr][i].rate;
+          }
+        }
+
       }
+    }
+
+    if (rate>0){
       //现在开始计算整个网体的奖励，从直推开始一路递归下去
+      //有一个问题没有考虑到，就是升级以前的网体收益是不同的。！！！
       uint256 dayLockBalance = queryLockBalance(_addr,_queryTime);
       return levelBonus(dayLockBalance,rate,_addr,_queryTime);
 
@@ -474,10 +536,10 @@ contract KOLPromote is Ownable{
 
   }
   /**
-   * @title 递归计算直推网体奖励
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 递归计算直推网体奖励
+   * dev visit: https://github.com/jackoelv/KOL/
   */
-  function levelBonus(uint256 _ancientBalance,uint8 _rate,address _addr,uint256 _queryTime) private view return(uint256){
+  function levelBonus(uint256 _ancientBalance,uint8 _rate,address _addr,uint256 _queryTime) private view returns(uint256){
     uint256 bonus;
 
     if ( ChildAddrs[_addr].length > 0 ){
@@ -495,10 +557,13 @@ contract KOLPromote is Ownable{
   }
 
   /**
-   * @title 录入KOL的收盘价
-   * @dev visit: https://github.com/jackoelv/KOL/
+   * title 录入KOL的收盘价
+   * dev visit: https://github.com/jackoelv/KOL/
   */
-  function putClosePrice() onlyNodes public{
+  function putClosePrice(uint256 price) onlyNodes public{
+    //录入的价格为4位小数
+    uint256 yestodayLastSecond = now.sub(now.sub(begin) % 86400) - 1;
+    ClosePrice[yestodayLastSecond] = price;
 
   }
 
