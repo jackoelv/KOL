5ef808a7dabfa83a0c19528267e87571460923ae
jack <jackoelv@qq.com>
2020-05-01 08:46:11 +0800

    before locknode

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
 var KOLVote = artifacts.require("KOLVote");
-// var USDT = artifacts.require("TetherToken");
-// var BTT = artifacts.require("BTT");
 var KOLP = artifacts.require("KOLPro");
 module.exports = function(deployer) {
   deployer.deploy(KOLVote,{gas: 6700000});
-  deployer.deploy(KOLP,"0xcb3aA0A1125f60cbb476eeF1daF17e49b9F3f154","0xe2ec44549b3ec410b4a41491b4c45541efbd7b8b",1587682800,1590274800,{gas: 6700000});
+  deployer.deploy(KOLP,"0xcb3aA0A1125f60cbb476eeF1daF17e49b9F3f154","0x095F979C8f89BbC137391d3cA0C64597002A5a18",1587682800,1590274800,{gas: 6700000});
   // deployer.deploy(TetherToken,200000000000,"Tether USD","USDT",6);
   //deployer.deploy(KOLVote,{gas: 6700000});
   //deployer.deploy(USDT,200000000000,"Tether USD","USDT",6,{gas: 6700000});
---
+++ test/tjoinresult.js
@@ -0,0 +1,58 @@
+const KK= artifacts.require("KOLVote");
+var KOLP = artifacts.require("KOLPro");
+function dateFtt(dd)
+{ //author: meizz
+   dd = dd +"000";
+   var fmt = "yyyy-MM-dd hh:mm:ss";
+   var date = new Date(parseInt(dd));
+   var o = {
+   "M+" : date.getMonth()+1,     //月份
+   "d+" : date.getDate(),     //日
+   "h+" : date.getHours(),     //小时
+   "m+" : date.getMinutes(),     //分
+   "s+" : date.getSeconds(),     //秒
+   "q+" : Math.floor((date.getMonth()+3)/3), //季度
+   "S" : date.getMilliseconds()    //毫秒
+   };
+   if(/(y+)/.test(fmt))
+   fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
+   for(var k in o)
+   if(new RegExp("("+ k +")").test(fmt))
+   fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+   return fmt;
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+contract("testjoin",accounts => {
+    before(async () => {
+    //构建合约(可以设置两种方式)
+    //01
+    let k = await KK.at("0xcb3aA0A1125f60cbb476eeF1daF17e49b9F3f154");
+    let p = await KOLP.at("0xd9E4B0CC779dE12871527Cb21d5F55d7D7e611E2");
+
+    for (var m = 1; m<4; m++){
+      console.log("*****************************************");
+      console.log("accounts is: " + m);
+      let len = await p.getHistoryTeamBonus(accounts[m],0);
+      for (var i = 0; i<len[4]; i++){
+        let result = await p.getHistoryTeamBonus(accounts[m],i);
+        console.log("i is: " + i);
+        console.log("the unix time is:" + web3.utils.BN(result[0]));
+        console.log("the time is:" + dateFtt(result[0]));
+        console.log("theDayTeamBonus :" +web3.utils.fromWei(result[1],"ether"));
+        console.log("totalTeamBonus :" +web3.utils.fromWei(result[2],"ether"));
+        console.log("theDayRate :" +web3.utils.BN(result[3]));
+      }
+      console.log("########################################");
+
+    }
+
+
+
+
+    });
+    it('Check whether the contract has been issued successfully', async () => {
+      console.log("测试合约的操作:");
+    });
+});
